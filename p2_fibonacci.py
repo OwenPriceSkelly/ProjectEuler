77bd8cced78630d5bba6f564e479184985b645b3
OwenPriceSkelly <priceo19@up.edu>
2019-08-17 12:31:10 -0700

    Update p2_fibonacci.py

--- p2_fibonacci.py
+++ p2_fibonacci.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 import math
-import time
-# problem 2
-# By considering the terms in the Fibonacci sequence whose
-# values do not exceed four million, find the sum of the
-# even-valued terms.
-def fib(x,y,n,evens):
-    z = x + y
-    if z >= n:
-        return
-    if z % 2 == 0:
-        evens.append(z)
-    fib(y,z,n,evens)
+
+def memo(f):
+    cache = []
+    def memoize(x):
+        if x not in cache:
+            cache[x] = f(x)
+        return cache[x]
+    return memoize
+
+@memo
+def fib(n):
+    return n if n < 2 else fib(n-1) + fib(n-2)
+
+
+
+
+def result(n=4000000):
+    """
+    Problem 2
+    By considering the terms in the Fibonacci sequence whose
+    values do not exceed four million, find the sum of the
+    even-valued terms.
+    """
+    i = sum = 0
+    while fib(i) < n:
+        if fib(i) % 2 == 0:
+            sum += fib(i)
+        i += 1
+    return sum  
